599ab88902b63cc25277bb03ad6a9ce889261a7a
Dean Wissing <deanwissing@gmail.com>
2025-03-09 20:34:21 -0400

    Update PreflightCheckModal to display airline and gate details dynamically

--- src/components/PreflightCheckModal.vue
+++ src/components/PreflightCheckModal.vue
@@ -2,7 +2,10 @@
   <q-dialog v-model="show">
     <q-card>
       <div class="row justify-center">
-        <div class="text-h6 q-ml-sm">Preflight Check</div>
+        <div class="text-h6 q-ml-sm">
+          Preflight Check {{ store.selected_airline.abbreviation }} Gate
+          {{ store.selected_gate.gate_number }}
+        </div>
         <q-space></q-space>
         <q-btn flat round icon="mdi-close" @click="show = false" />
       </div>
@@ -14,6 +17,7 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import SinglePreflightCheck from './SinglePreflightCheck.vue'
-
+import { useAppState } from '../stores/appState'
+const store = useAppState()
 const show = ref(true)
 </script>
